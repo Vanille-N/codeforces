44168abb9408368df318d4908a0ff19844ef010e
Vanille-N <neven.villani@gmail.com>
2020-08-21 13:23:03 +0200

    automatic fetcher script

---
+++ codefetch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# *** CODEFETCH ***
+#
+# Automatic profile updater for Codeforces
+
+web = "https://codeforces.com/"
+handle = "Zwgtwz"
+profile = web + "profile/" + handle
+contests = web + "contests/with/" + handle
+submissions = web + "submissions/" + handle
+
