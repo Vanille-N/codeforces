4de34082c0c9966e9e0fd525af2b5ee844efeb99
Vanille-N <neven.villani@gmail.com>
2020-08-21 13:37:22 +0200

    get current rating

--- codefetch.py
+++ codefetch.py
@@ -4,9 +4,18 @@
 #
 # Automatic profile updater for Codeforces
 
+import requests
+import re
+
 web = "https://codeforces.com/"
 handle = "Zwgtwz"
 profile = web + "profile/" + handle
 contests = web + "contests/with/" + handle
 submissions = web + "submissions/" + handle
 
+f = requests.get(profile)
+text = f.text.replace("\n", " ").replace("\r", "")
+#print(f.text)
+
+current_rating = int(re.search("Contest rating.*?([0-9]+)", text).group(1))
+print(current_rating)
