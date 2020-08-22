ac5c35c273c85416001c77f867415e25f9df6b87
Vanille-N <neven.villani@gmail.com>
2020-08-22 09:32:04 +0200

    title getter

--- codefetch.py
+++ codefetch.py
@@ -7,6 +7,30 @@
 import requests
 import re
 
+def get_title(rating):
+    if rating == 0:
+        return "Unrated", "white"
+    elif rating <= 1200:
+        return "Newbie", "lightgray"
+    elif rating <= 1400:
+        return "Pupil", "green"
+    elif rating <= 1600:
+        return "Specialist", "cyan"
+    elif rating <= 1900:
+        return "Expert", "blue"
+    elif rating <= 2100:
+        return "Candidate Master", "hotpink"
+    elif rating <= 2300:
+        return "Master", "gold"
+    elif rating <= 2400:
+        return "International Master", "yellow"
+    elif rating <= 2600:
+        return "Grandmaster", "red"
+    elif rating <= 3000:
+        return "International Grandmaster", "crimson"
+    else:
+        return "Legendary Grandmaster", "crimson"
+
 web = "https://codeforces.com/"
 handle = "Zwgtwz"
 profile = web + "profile/" + handle
