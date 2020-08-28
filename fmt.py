47f724324016d39d7aa3536d26a1e889a1ccfb81
Vanille-N <neven.villani@gmail.com>
2020-08-28 14:42:10 +0200

    corrections

--- fmt.py
+++ fmt.py
@@ -7,7 +7,7 @@ prev_rating = 0
 next_rating = 0
 main = main_template
 
-for id in participations:
+for cnt,id in enumerate(participations):
     try:
         exec(open("{}/.data.py".format(id)).read())
     except FileNotFoundError:
@@ -19,11 +19,11 @@ for id in participations:
     next_title, next_color = find_title(next_rating)
     report = report_template.format(
         contest_name=contest_name,
-        participation_badge=count_template.format(number=num),
+        participation_badge=count_template.format(number=cnt+1),
         rank_badge=rank_template.format(rank=rank),
         score=(
             penalty_template.format(penalty=penalty) if educ
-            else score_template.format(score=score)
+            else score_template.format(points=points)
             ),
         prev_rating=rating_template.format(title=prev_title, rating=prev_rating, color=prev_color),
         next_rating=rating_template.format(title=next_title, rating=next_rating, color=next_color),
