c145f088d081b00ad66b161c1629c19f1e9caa7f
Vanille-N <neven.villani@gmail.com>
2020-08-28 13:42:15 +0200

    data on 661

---
+++ 661/.data.py
@@ -0,0 +1,57 @@
+eval(open("../.meta.py").read())
+
+contest_name = "Codeforces Round #661 (Div. 3)"
+number = 1399
+rank = 2242
+type = "educ"
+
+penalty = 195
+rating_change = 535
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Remove Smallest",
+        "time": (0,12),
+        "penalty": 12,
+    }, {
+        "problem": "B",
+        "name": "Gifts Fixing",
+        "time": (0,31),
+        "penalty": 31,
+    }, {
+        "problem": "C",
+        "name": "Boats Competition",
+        "time": (1,7),
+        "penalty": 67,
+    }, {
+        "problem": "D",
+        "name": "Binary String to Subsequences",
+        "time": (1,25),
+        "penalty": 85,
+    }
+]
+
+submissions = [
+    {
+        "problem": "A",
+        "id": 88973817,
+        "status": accepted(),
+    }, {
+        "problem": "B",
+        "id": 88989375,
+        "status": wrong_ans(1),
+    }, {
+        "problem": "B",
+        "id": 88994776,
+        "status": accepted(),
+    }, {
+        "problem": "C",
+        "id": 89020550,
+        "status": accepted(),
+    }, {
+        "problem": "D",
+        "id": 89030954,
+        "status": accepted(),
+    }
+]
