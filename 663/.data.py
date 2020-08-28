17c19deae29351ff0d0b0fe23302b0ee2b6cbc4a
Vanille-N <neven.villani@gmail.com>
2020-08-28 14:42:02 +0200

    data for 663

---
+++ 663/.data.py
@@ -0,0 +1,46 @@
+contest_name = "Codeforces Round #661 (Div. 3)"
+num = 1391
+rank = 2863
+educ = False
+
+points = 2101
+rating_change = 388
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Suborrays",
+        "time": (0,15),
+        "points": (470,500)
+    }, {
+        "problem": "B",
+        "name": "Conveyor Belt",
+        "time": (0,28),
+        "points": (666,750)
+    }, {
+        "problem": "C",
+        "name": "Cyclic Permutations",
+        "time": (0,57),
+        "points": (965,1250)
+    }
+]
+
+submissions = [
+    {
+        "problem": "A",
+        "id": 89423384,
+        "status": accepted(),
+    }, {
+        "problem": "B",
+        "id": 89430043,
+        "status": accepted(),
+    }, {
+        "problem": "C",
+        "id": 89439136,
+        "status": wrong_ans(1),
+    }, {
+        "problem": "C",
+        "id": 89439267,
+        "status": accepted(),
+    }
+]
