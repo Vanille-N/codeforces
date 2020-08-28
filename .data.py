4630764f9676f5f4f59efe3d0bc9f8c530855e47
Vanille-N <neven.villani@gmail.com>
2020-08-28 12:21:37 +0200

    after consideration, this is the only relevant info

--- .data.py
+++ .data.py
@@ -1,34 +1,9 @@
 handle = "Zwgtwz"
 
 participations = [
-    {
-        "id": "661",
-        "name": "Codeforces Round #661 (Div. 3)",
-        "rank": 2242,
-        "gain": 535,
-    },
-    {
-        "id": "663",
-        "name": "Codeforces Round #663 (Div. 2)",
-        "rank": 2863,
-        "gain": 388,
-    },
-    {
-        "id": "G10",
-        "name": "Codeforces Global Round 10",
-        "rank": 2169,
-        "gain": 317,
-    },
-    {
-        "id": "665",
-        "name": "Codeforces Round #666 (Div. 2)",
-        "rank": 4974,
-        "gain": 105,
-    },
-    {
-        "id": "E94",
-        "name": "Educational Codeforces Round 94 (Div. 2)",
-        "rank": 1766,
-        "gain": 164,
-    },
+    "661",
+    "663",
+    "G10",
+    "665",
+    "E94",
 ]
