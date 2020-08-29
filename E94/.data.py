b9d0e989787854f115be6715d7ed94c1762c0391
Vanille-N <neven.villani@gmail.com>
2020-08-29 10:26:37 +0200

    want more data ?

---
+++ E94/.data.py
@@ -0,0 +1,58 @@
+contest_name = "Educational Codeforces Round 94 (Div. 2)"
+num = 1400
+rank = 1766
+educ = True
+
+penalty = 195
+rating_change = 164
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "String Similarity",
+        "time": (0,10),
+        "penalty": 10,
+    }, {
+        "problem": "B",
+        "name": "RPG Protagonist",
+        "time": (0,39),
+        "penalty": 49,
+    }, {
+        "problem": "C",
+        "name": "Binary String Reconstruction",
+        "time": (1,14),
+        "penalty": 84,
+    }
+]
+
+submissions = [
+    {
+        "problem": "A",
+        "id": 90918258,
+        "status": accepted(),
+    }, {
+        "problem": "B",
+        "id": 90934965,
+        "status": wrong_ans(2),
+    }, {
+        "problem": "B",
+        "id": 90937466,
+        "status": accepted(),
+    }, {
+        "problem": "C",
+        "id": 90947411,
+        "status": wrong_ans(2),
+    }, {
+        "problem": "C",
+        "id": 90948212,
+        "status": wrong_ans(1),
+    }, {
+        "problem": "C",
+        "id": 90957144,
+        "status": accepted(),
+    }, {
+        "problem": "D",
+        "id": 90981612,
+        "status": wrong_ans(2),
+    }
+]
