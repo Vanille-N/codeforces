9acbc5753c03bdef0a49ce976b0d89c8b476113a
Vanille-N <neven.villani@gmail.com>
2020-08-29 10:26:44 +0200

    here's more data.

---
+++ G10/.data.py
@@ -0,0 +1,67 @@
+contest_name = "Global Codeforces Round 10"
+num = 1392
+rank = 2203
+educ = False
+
+points = 2941
+rating_change = 317
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Omkar and Password",
+        "time": (0,12),
+        "points": (484,500)
+    }, {
+        "problem": "B",
+        "name": "Omkar and Infinity Clock",
+        "time": (0,31),
+        "points": (688,750),
+    }, {
+        "problem": "C",
+        "name": "Omkar and Waterslide",
+        "time": (0,41),
+        "points": (891,1000),
+    }, {
+        "problem": "D",
+        "name": "Omkar and Bed Wars",
+        "time": (1,58),
+        "points": (878,1500),
+    }
+]
+
+submissions = [
+    {
+        "problem": "A",
+        "id": 90099352,
+        "status": accepted(),
+    }, {
+        "problem": "B",
+        "id": 90114207,
+        "status": accepted(),
+    }, {
+        "problem": "C",
+        "id": 90120835,
+        "status": accepted(),
+    }, {
+        "problem": "D",
+        "id": 90135792,
+        "status": wrong_ans(2, pre=True),
+    }, {
+        "problem": "D",
+        "id": 90147918,
+        "status": wrong_ans(2, pre=True),
+    }, {
+        "problem": "D",
+        "id": 90149075,
+        "status": wrong_ans(2, pre=True),
+    }, {
+        "problem": "D",
+        "id": 90151859,
+        "status": wrong_ans(1, pre=True),
+    }, {
+        "problem": "D",
+        "id": 90152128,
+        "status": accepted(),
+    }
+]
