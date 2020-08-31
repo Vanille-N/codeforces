79eb1ca7021552c05b603b504be7932b4a1eb0a1
Vanille-N <neven.villani@gmail.com>
2020-08-31 09:55:28 +0200

    solutions

--- 666/.data.py
+++ 666/.data.py
@@ -5,3 +5,27 @@ educ = False
 
 points = 3206
 rating_change = 168
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Juggling Letters",
+        "time": (0,8),
+        "points": (484,500)
+    }, {
+        "problem": "B",
+        "name": "Power Sequence",
+        "time": (0,31),
+        "points": (876,1000)
+    }, {
+        "problem": "C",
+        "name": "Multiples of Length",
+        "time": (1,18),
+        "points": (810,1250)
+    }, {
+        "problem": "D",
+        "name": "Stoned Game",
+        "time": (1,42),
+        "points": (1036,1750),
+    }
+]
