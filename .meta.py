fa77e56684698bc16e1523a4b53d1f40ed9bc9c4
Vanille-N <neven.villani@gmail.com>
2020-09-03 10:37:12 +0200

    changed template: rating change on main page

--- .meta.py
+++ .meta.py
@@ -14,7 +14,7 @@ All participations done in Rust.
 
 """
 
-main_entry = "* [{contest_name}]({contest_url}) as ![]({badge})\n"
+main_entry = "* [{contest_name}]({contest_url}) as ![]({badge}) ![]({rating_change})\n"
 
 contest_url_template = web + "/contest/{num}"
 
