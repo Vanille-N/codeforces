e01b928b3df0b3d99e3fef12c3c130d868152b08
Vanille-N <neven.villani@gmail.com>
2020-09-08 09:10:51 +0200

    initial algorithm just had an overflow on the last sum :'(

---
+++ 668/B_array-cancellation_bis.rs
@@ -0,0 +1,103 @@
+#![overflow_checks(on)]
+
+fn main() {
+    let mut scan = Scanner::default();
+    let t: u64 = scan.item();
+    for _ in 0..t {
+        let n: usize = scan.item();
+        let a: Vec<i64> = scan.vec(n);
+        println!("{}", cost(a));
+    }
+}
+
+fn cost(mut a: Vec<i64>) -> u64 {
+    let mut i = 0;
+    let mut j = 0;
+    let n = a.len();
+    while j < n {
+        if i == j {
+            j += 1;
+        } else if a[i] <= 0 {
+            i += 1;
+        } else if a[j] >= 0 {
+            j += 1;
+        } else {
+            let m = a[i].min(a[j].abs());
+            a[i] -= m;
+            a[j] += m;
+        }
+    }
+    a.iter().map(|x| *x.max(&0) as u64).sum()
+}
+
+// TEMPLATE
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn atomic_scan<T: AtomicScannable>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+trait AtomicScannable: std::str::FromStr {}
+impl AtomicScannable for u64 {}
+impl AtomicScannable for i64 {}
+impl AtomicScannable for f64 {}
+impl AtomicScannable for usize {}
+impl AtomicScannable for isize {}
+impl AtomicScannable for String {}
+trait Scan<T> {
+    fn item(&mut self) -> T;
+}
+impl<T: AtomicScannable> Scan<T> for Scanner {
+    fn item(&mut self) -> T {
+        self.atomic_scan()
+    }
+}
+impl<T, U> Scan<(T, U)> for Scanner where Scanner: Scan<T> + Scan<U> {
+    fn item(&mut self) -> (T, U) {
+        (self.item(), self.item())
+    }
+}
+impl<T, U, V> Scan<(T, U, V)> for Scanner where Scanner: Scan<T> + Scan<U> + Scan<V> {
+    fn item(&mut self) -> (T, U, V) {
+        (self.item(), self.item(), self.item())
+    }
+}
+trait ScanVec<T> {
+    fn vec(&mut self, n: usize) -> Vec<T>;
+}
+impl<T> ScanVec<T> for Scanner where Scanner: Scan<T> {
+    fn vec(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.item()).collect()
+    }
+}
+impl ScanVec<bool> for Scanner {
+    fn vec(&mut self, n: usize) -> Vec<bool> {
+        let s: String = self.item();
+        let v: Vec<bool> = s.chars().map(|c| c == '1').collect();
+        if v.len() != n {
+            panic!("Wrong length for Vec<bool>: got {}, expected {}", v.len(), n);
+        }
+        v
+    }
+}
+impl ScanVec<char> for Scanner {
+    fn vec(&mut self, n: usize) -> Vec<char> {
+        let s: String = self.item();
+        let v: Vec<char> = s.chars().collect();
+        if v.len() != n {
+            panic!("Wrong length for Vec<char>: got {}, expected {}", v.len(), n);
+        }
+        v
+    }
+}
