59c26352a91dfc7eea2ac5b0216711bbd0ed8e84
Vanille-N <neven.villani@gmail.com>
2020-09-20 07:32:31 +0200

    Yikes, missed the beginning and didn't have enough time to do anything.

---
+++ 671/A_digit-game.rs
@@ -0,0 +1,97 @@
+
+fn main() {
+    let mut scan = Scanner::default();
+    let t: usize = scan.item();
+    for _ in 0..t {
+        let n: usize = scan.item();
+        let chrs: Vec<char> = scan.vec(n);
+        println!("{}", winner(n, chrs));
+    }
+}
+
+fn winner(n: usize, chrs: Vec<char>) -> u8 {
+    let selected = chrs.iter()
+        .map(|d| d.to_digit(10).unwrap() as usize)
+        .rev()
+        .enumerate()
+        .filter(|(k, _)| k % 2 != n % 2)
+        .filter(|(_, d)| d % 2 == n % 2)
+        .count();
+    if selected > 0 {
+        if n % 2 != 0 { 1 } else { 2 }
+    } else {
+        if n % 2 != 0 { 2 } else { 1 }
+    }
+}
+
+// TEMPLATE
+
+#[derive(Default)]
+struct Scanner {
+    buffer: Vec<String>
+}
+impl Scanner {
+    fn atomic_scan<T: AtomicScannable>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+trait AtomicScannable: std::str::FromStr {}
+impl AtomicScannable for u64 {}
+impl AtomicScannable for i64 {}
+impl AtomicScannable for f64 {}
+impl AtomicScannable for usize {}
+impl AtomicScannable for isize {}
+impl AtomicScannable for String {}
+trait Scan<T> {
+    fn item(&mut self) -> T;
+}
+impl<T: AtomicScannable> Scan<T> for Scanner {
+    fn item(&mut self) -> T {
+        self.atomic_scan()
+    }
+}
+impl<T, U> Scan<(T, U)> for Scanner where Scanner: Scan<T> + Scan<U> {
+    fn item(&mut self) -> (T, U) {
+        (self.item(), self.item())
+    }
+}
+impl<T, U, V> Scan<(T, U, V)> for Scanner where Scanner: Scan<T> + Scan<U> + Scan<V> {
+    fn item(&mut self) -> (T, U, V) {
+        (self.item(), self.item(), self.item())
+    }
+}
+trait ScanVec<T> {
+    fn vec(&mut self, n: usize) -> Vec<T>;
+}
+impl<T> ScanVec<T> for Scanner where Scanner: Scan<T> {
+    fn vec(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.item()).collect()
+    }
+}
+impl ScanVec<bool> for Scanner {
+    fn vec(&mut self, n: usize) -> Vec<bool> {
+        let s: String = self.item();
+        let v: Vec<bool> = s.chars().map(|c| c == '1').collect();
+        if v.len() != n {
+            panic!("Wrong length for Vec<bool>: got {}, expected {}", v.len(), n);
+        }
+        v
+    }
+}
+impl ScanVec<char> for Scanner {
+    fn vec(&mut self, n: usize) -> Vec<char> {
+        let s: String = self.item();
+        let v: Vec<char> = s.chars().collect();
+        if v.len() != n {
+            panic!("Wrong length for Vec<char>: got {}, expected {}", v.len(), n);
+        }
+        v
+    }
+}
