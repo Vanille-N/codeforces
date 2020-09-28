151507b05fc5c24b42b5188697d6fd1b3060744b
Vanille-N <neven.villani@gmail.com>
2020-09-28 07:55:02 +0200

    add data

---
+++ 673/.data.py
@@ -0,0 +1,37 @@
+name = "Codeforces Round #666 (Div. 2)"
+date = (2020,9,28)
+rank = 3134
+educ = False
+
+points = 1294
+rating_change = -15
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Copy-paste",
+        "time": (0,21),
+        "points": (458,500),
+    }, {
+        "problem": "B",
+        "name": "Two Arrays",
+        "time": (1,41),
+        "points": (836,1000),
+    }
+]
+
+submissions = [
+    {
+        "problem": "A",
+        "id": 93982235,
+        "status": verdict.accepted(),
+    }, {
+        "problem": "B",
+        "id": 93991932,
+        "status": verdict.accepted(),
+    }, {
+        "problem": "C",
+        "id": 94009983,
+        "status": verdict.wrong_ans(5),
+    }
+]
