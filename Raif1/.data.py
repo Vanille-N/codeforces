0a79bebef3fe24066e4ba0ca4707b11d0fa7663e
Vanille-N <neven.villani@gmail.com>
2020-10-24 09:32:29 +0200

    solutions for Raif1

---
+++ Raif1/.data.py
@@ -0,0 +1,26 @@
+name = "Codeforces Raif Round 1 (Div. 1 + Div. 2)"
+date = (2020,10,17)
+rank = 2521
+educ = False
+
+points = 2292
+rating_change = 74
+
+solutions = [
+    {
+        "problem": "A",
+        "name": "Box is Pull",
+        "time": (0,9),
+        "points": (486,500)
+    }, {
+        "problem": "B",
+        "name": "Belted Rooms",
+        "time": (0,27),
+        "points": (914,1000),
+    }, {
+        "problem": "C",
+        "name": "ABBB",
+        "time": (0,34),
+        "points": (892,1000),
+    }
+]
